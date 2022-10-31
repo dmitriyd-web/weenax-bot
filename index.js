85644e7140244f5393cd389007ee51c62ee5e19e
dmitriyd-web <yadimon1989@gmail.com>
2022-10-31 17:49:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
-server.listen(server_port, server_host, function() {
-    console.log('Listening on port %d', server_port);
-});
 const { Telegraf } = require('telegraf');
 
 const bot = new Telegraf('5720289702:AAGeqb6UHfgodGxCvKyvCGwyHew8eLJ6bb4');
